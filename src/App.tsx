e3670321745e1d4bbd12f7209958101c91a47cc9
fumiya <altema1217bbfumi@live.jp>
2021-10-05 21:22:17 +0900

    feat: emotion環境

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Stack from '@mui/material/Stack'
 import DeleteIcon from '@mui/icons-material/Delete'
 import AlarmIcon from '@mui/icons-material/Alarm'
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Button variant="contained">Hello, world.</Button>
       <Stack direction="row" spacing={1}>
         <IconButton aria-label="delete">
